15463fadfa4eec33c39e9b429621f1d709a7074e
Trezy <tre@trezy.com>
2017-03-21 20:56:40 -0500

    Preferences pane updates
    * Add support for checkbox fields
    * Convert preferences pane JS to a class

--- preferences/index.js
+++ preferences/index.js
@@ -5,39 +5,55 @@ let Config = require('electron-config')
 
 
 
-let config = new Config
-let windows = BrowserWindow.getAllWindows()
+new class PreferencesPane {
+  constructor () {
+    this.config = new Config
+    this.windows = BrowserWindow.getAllWindows()
 
+    this.handleInput = this.handleInput.bind(this)
 
+    this.initialize()
+  }
 
+  handleInput (event) {
+    let input = event.target
+    let prefName = input.getAttribute('name')
 
+    if (input.getAttribute('type') === 'checkbox') {
+      this.config.set(prefName, input.checked)
 
-windows.forEach(win => {
-  let rootEl = document.querySelector('html')
+    } else {
+      this.config.set(prefName, input.value)
+    }
+  }
 
-  win.setSize(500, document.body.clientHeight + 22)
-})
+  initialize () {
+    this.windows.forEach(win => {
+      let rootEl = document.querySelector('html')
 
-function handleInput (event) {
-  let input = event.target
-  let prefName = input.getAttribute('name')
+      win.setSize(500, document.body.clientHeight + 22)
+    })
 
-  config.set(prefName, input.value)
-}
+    let inputs = document.querySelectorAll('input')
 
-function setInputValue (input) {
-  let prefName = input.getAttribute('name')
-  let pref = config.get(prefName)
+    for (let i = 0, length = inputs.length; i < length; i++) {
+      let input = inputs[i]
 
-  input.value = pref || ''
-}
+      this.setInputValue(input)
 
-let inputs = document.querySelectorAll('input')
+      if (input.getAttribute('type') === 'checkbox') {
+        input.addEventListener('change', this.handleInput)
 
-for (let i = 0, length = inputs.length; i < length; i++) {
-  let input = inputs[i]
+      } else {
+        input.addEventListener('input', this.handleInput)
+      }
+    }
+  }
 
-  setInputValue(input)
+  setInputValue (input) {
+    let prefName = input.getAttribute('name')
+    let pref = this.config.get(prefName)
 
-  input.addEventListener('input', handleInput)
+    input.value = pref || ''
+  }
 }
