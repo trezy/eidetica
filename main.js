40ac0fd66d629fb31354640f8c0d3e08aac0d778
Trezy <tre@trezy.com>
2017-03-27 20:42:37 -0500

    [#10] Move autoupdate out of the initializer
    Having the autoupdater in the initializer meant it was possible that a race condition could be caused wherein the autoupdater may miss the application's `ready` event, causing the autoupdater to stall the rest of the app.

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const {
   systemPreferences,
   Tray,
 } = require('electron')
-const { autoUpdater } = require('electron-updater')
 const Config = require('electron-config')
 const notify = require('electron-main-notification')
 const fs = require('fs')
@@ -25,6 +24,34 @@ const url = require('url')
 
 // Setup development mode
 let developmentMode = process.env.NODE_ENV === 'development'
+
+
+
+
+
+// Setup autoupdater
+if (!developmentMode) {
+  const { autoUpdater } = require('electron-updater')
+
+  autoUpdater.logger = log
+  autoUpdater.logger.transports.file.level = 'info'
+
+  autoUpdater.on('update-available', () => {
+    notify('Eidetica update available', {
+      body: `We're downloading the update now and will restart when we're finished.`
+    })
+  })
+
+  autoUpdater.on('update-downloaded', () => {
+    notify('Restarting Eidetica', {
+      body: `We're restarting Eidetica so you can have all the nifty new features. 😉`
+    })
+
+    autoUpdater.quitAndInstall()
+  })
+
+  autoUpdater.checkForUpdates()
+}
 
 
 
@@ -134,26 +161,9 @@ new class App {
   }
 
   initialize () {
-    if (!developmentMode) {
-      autoUpdater.logger = log
-      autoUpdater.logger.transports.file.level = 'info'
 
-      autoUpdater.on('update-available', () => {
-        notify('Eidetica update available', {
-          body: `We're downloading the update now and will restart when we're finished.`
-        })
-      })
 
-      autoUpdater.on('update-downloaded', () => {
-        notify('Restarting Eidetica', {
-          body: `We're restarting Eidetica so you can have all the nifty new features. 😉`
-        })
 
-        autoUpdater.quitAndInstall()
-      })
-
-      autoUpdater.checkForUpdates()
-    }
 
     let trayIconPath
 
