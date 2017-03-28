70c81e7e52d471cebea53c39c5c340bf76ae3eb1
Trezy <tre@trezy.com>
2017-03-28 00:19:18 -0500

    [#24] Refactor preferences JS again
    * Use switches in generic functions so we can bind multiple types of inputs with a single method
    * Explicitly declare the inputs that should pass their values directly through to the config

--- panes/preferences.js
+++ panes/preferences.js
@@ -10,7 +10,7 @@ new class {
     this.config = new Config
 
     this.handleLaunchAtLoginChange = this.handleLaunchAtLoginChange.bind(this)
-    this.handleTextInput = this.handleTextInput.bind(this)
+    this.handleInput = this.handleInput.bind(this)
 
     this.initialize()
   }
@@ -26,23 +26,80 @@ new class {
     })
   }
 
-  handleTextInput (event) {
+  handleInput (event) {
     let input = event.target
     let prefName = input.getAttribute('name')
+    let value
 
-    this.config.set(prefName, input.value)
+    switch (input.getAttribute('type')) {
+      case 'checkbox':
+        value = input.checked
+        break
+
+      case 'email':
+      case 'number':
+      case 'password':
+      case 'url':
+      case 'text':
+      default:
+        value = input.value
+    }
+
+    this.config.set(prefName, value)
   }
 
   initialize () {
-    this.setupTextInputs()
+    this.setupInputs([
+      'host',
+      'password',
+      'path',
+      'port',
+      'url',
+      'username',
+    ])
+
     this.setupLaunchAtLoginInput()
   }
 
-  setTextInputValue (textInput) {
-    let prefName = textInput.getAttribute('name')
-    let pref = this.config.get(prefName)
+  linkInputToConfig (input) {
+    let eventType
 
-    textInput.value = pref || ''
+    this.setInputValue(input)
+
+    switch (input.getAttribute('type')) {
+      case 'checkbox':
+        eventType = 'change'
+        break
+
+      case 'email':
+      case 'number':
+      case 'password':
+      case 'url':
+      case 'text':
+      default:
+        eventType = 'input'
+    }
+
+    input.addEventListener(eventType, this.handleInput)
+  }
+
+  setInputValue (input) {
+    let pref = input.getAttribute('name')
+    let value = this.config.get(pref)
+
+    switch (input.getAttribute('type')) {
+      case 'checkbox':
+        input.checked = value
+        break
+
+      case 'email':
+      case 'number':
+      case 'password':
+      case 'url':
+      case 'text':
+      default:
+        input.value = value || ''
+    }
   }
 
   setupLaunchAtLoginInput () {
@@ -54,15 +111,11 @@ new class {
     input.addEventListener('change', this.handleLaunchAtLoginChange)
   }
 
-  setupTextInputs () {
-    let textInputs = document.querySelectorAll('[type=number], [type=password], [type=text], [type=url]')
+  setupInputs (inputs) {
+    inputs.forEach(inputID => {
+      let input = document.querySelector(`#${inputID}`)
 
-    for (let i = 0, length = textInputs.length; i < length; i++) {
-      let textInput = textInputs[i]
-
-      this.setTextInputValue(textInput)
-
-      textInput.addEventListener('input', this.handleTextInput)
-    }
+      this.linkInputToConfig(input)
+    })
   }
 }
