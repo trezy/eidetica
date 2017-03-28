d569082784faf0276962038c41b8824f4c362344
Trezy <tre@trezy.com>
2017-03-28 14:36:51 -0500

    [#28] Abstract main.js
    `main.js` was getting *way* too big. I've separated almost all of the functions into their own modules to be imported. This will make it easier when we are ready to support other platforms, as we'll be able to conditionally require modules based on platform.

---
+++ modules/common/copyFile.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const log = require('electron-log')
+
+
+
+
+
+module.exports = function (from, to) {
+  return new Promise((resolve, reject) => {
+    let readStream = fs.createReadStream(from)
+    let writeStream = fs.createWriteStream(to)
+
+    readStream.on('error', error => {
+      log.error('Error reading screenshot file:', error)
+      reject(error)
+    })
+
+    writeStream.on('error', error => {
+      log.error('Error copying screenshot file:', error)
+      reject(error)
+    })
+
+    writeStream.on('close', () => {
+      log.info('Copied screenshot to temp directory')
+      resolve()
+    })
+
+    readStream.pipe(writeStream)
+  })
+}
---
+++ modules/common/setupApplicationMenu.js
@@ -0,0 +1,22 @@
+const { Menu } = require('electron')
+
+
+
+
+
+module.exports = function () {
+  Menu.setApplicationMenu(Menu.buildFromTemplate([{
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { role: 'selectall' },
+    ]
+  }]))
+}
