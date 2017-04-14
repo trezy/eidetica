8f17f6ec637e9b16e73a012af521405b7831f23f
Trezy <tre@trezy.com>
2017-04-13 23:38:57 -0500

    [#39] Wire up the uploads panel
    I moved all of the original stuff that had yet to be accounted for into
    the `Uploads` panel. It's all wired up, too, so changing any of these settings
    will work.

--- panes/components/AdvancedPane.js
+++ panes/components/AdvancedPane.js
@@ -16,58 +16,7 @@ import Pane from './Pane'
 export default class extends Pane {
   render () {
     return (
-      <div>
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <th><label htmlFor="host">Host:</label></th>
-                <td><input id="host" name="host" placeholder="eidetica.io" type="url" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="port">Port:</label></th>
-                <td><input id="port" name="port" placeholder="22" type="number" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="username">Username:</label></th>
-                <td><input id="username" name="username" placeholder="eidetica-user" type="text" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="password">Password:</label></th>
-                <td><input id="password" name="password" placeholder="Using SSH private key" type="password" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="path">Path:</label></th>
-                <td><input id="path" name="path" placeholder="/var/www/uploads/" type="text" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="url">URL:</label></th>
-                <td><input id="url" name="url" placeholder="http://eidetica.io/screenshot" type="url" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="hashBeforeUpload">Hash filenames before upload?</label></th>
-                <td><input id="hashBeforeUpload" name="hashBeforeUpload" type="checkbox" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="deleteAfterUpload">Delete screenshots after upload?</label></th>
-                <td><input id="deleteAfterUpload" name="deleteAfterUpload" type="checkbox" /></td>
-              </tr>
-
-              <tr>
-                <th><label htmlFor="launchAtLogin">Launch at login?</label></th>
-                <td><input id="launchAtLogin" name="launchAtLogin" type="checkbox" /></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
+      <div></div>
     )
   }
 }
--- panes/components/UploadPane.js
+++ panes/components/UploadPane.js
@@ -13,11 +13,148 @@ import Pane from './Pane'
 
 
 
+let config = new (require('electron-config'))
+
+
+
+
+
 export default class extends Pane {
+
+  /***************************************************************************\
+    Private methods
+  \***************************************************************************/
+
+  _handleChange (event) {
+    let newState = {}
+
+    newState[event.target.getAttribute('name')] = event.target.value
+
+    this.setState(newState)
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Public methods
+  \***************************************************************************/
+
+  componentWillUpdate (nextProps, nextState) {
+    for (let setting of nextState) {
+      if (nextState[setting] !== this.state[setting]) {
+        config.set(setting, nextState[setting])
+      }
+    }
+  }
+
+  constructor (props) {
+    super(props)
+
+    this._handleChange = this._handleChange.bind(this)
+
+    this.state = {
+      host: config.get('host'),
+      port: config.get('port'),
+      password: config.get('password'),
+      path: config.get('path'),
+      url: config.get('url'),
+      username: config.get('username'),
+    }
+  }
+
   render () {
     return (
       <div>
-        <Label>Blep</Label>
+        <table>
+          <tbody>
+            <tr>
+              <th>
+                <Label>Host:</Label>
+              </th>
+
+              <td>
+                <input
+                  name="host"
+                  onChange={this._handleChange}
+                  placeholder="eidetica.io"
+                  type="url"
+                  value={this.state.host} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <Label>Port:</Label>
+              </th>
+              <td>
+                <input
+                  onChange={this._handleChange}
+                  name="port"
+                  placeholder="22"
+                  type="number"
+                  value={this.state.port} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <Label>Username:</Label>
+              </th>
+              <td>
+                <input
+                  onChange={this._handleChange}
+                  name="username"
+                  placeholder="eidetica-user"
+                  type="text"
+                  value={this.state.username} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <Label>Password:</Label>
+              </th>
+              <td>
+                <input
+                  onChange={this._handleChange}
+                  name="password"
+                  placeholder="Using SSH private key"
+                  type="password"
+                  value={this.state.password} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <Label>Path:</Label>
+              </th>
+              <td>
+                <input
+                  onChange={this._handleChange}
+                  name="path"
+                  placeholder="/var/www/uploads/"
+                  type="text"
+                  value={this.state.path} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <Label>URL:</Label>
+              </th>
+              <td>
+                <input
+                  onChange={this._handleChange}
+                  name="url"
+                  placeholder="http://eidetica.io/screenshot"
+                  type="url"
+                  value={this.state.url} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
