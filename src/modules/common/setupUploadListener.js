28bf57a7c96ddacce8d7c6e9e43e87344a716c20
Trezy <tre@trezy.com>
2018-08-23 10:19:28 -0500

    Update the upload listener

--- src/modules/common/setupUploadListener.js
+++ src/modules/common/setupUploadListener.js
@@ -40,36 +40,30 @@ const setupUploadListener = () => {
   globalShortcut.register(config.get('shortcut'), async () => {
     const clipboardFileContents = clipboard.readBuffer('NSFilenamesPboardType').toString('utf8')
 
-      if (files.length > 1) {
-        promise = zipFiles(files)
+    const pasteboardTypes = [
+      'NSFileContentsPboardType',
+      'NSFilenamesPasteboardType',
+      'NSFilenamesPboardType',
+      'NSPasteboardTypeColor',
+      'NSPasteboardTypeFileURL',
+      'NSPasteboardTypeFont',
+      'NSPasteboardTypeHTML',
+      'NSPasteboardTypeMultipleTextSelection',
+      'NSPasteboardTypePDF',
+      'NSPasteboardTypePNG',
+      'NSPasteboardTypeRTF',
+      'NSPasteboardTypeRTFD',
+      'NSPasteboardTypeRuler',
+      'NSPasteboardTypeSound',
+      'NSPasteboardTypeString',
+      'NSPasteboardTypeTabularText',
+      'NSPasteboardTypeTextFinderOptions',
+      'NSPasteboardTypeTIFF',
+      'NSPasteboardTypeURL',
+    ]
 
-    if (clipboardFileContents) {
-      const matches = clipboardFileContents.match(/<string>.*<\/string>/gi)
-      const files = matches.map(string => entities.decode(string.replace(/<\/*string>/gi, '')))
-      let fileToUpload = null
-
-      if ((files.length > 1) || isDirectory(files[0])) {
-        try {
-          fileToUpload = await zipFiles(files)
-        } catch (error) {
-          log.info(error)
-        }
-        } else {
-        fileToUpload = await copyFile(files[0], generateTempFilepath(path.extname(files[0]), files[0]))
-        }
-
-      try {
-        uploadFile(fileToUpload)
-      } catch (error) {
-        log.error(error)
-      }
-    } else {
-      const file = await createTextFile(clipboard.readText())
-
-      uploadFile(file)
-    }
-  })
-}
+    for (const pasteboardType of pasteboardTypes) {
+      console.log(`${pasteboardType}:`, clipboard.readBuffer(pasteboardType).toString('utf8'))
       console.log('')
     }
     console.log('=== Clipboard data ===')
