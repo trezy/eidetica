e7230a8a87b189ed6982fad8d6c0097c6a8fd064
Trezy <tre@trezy.com>
2018-08-29 10:21:34 -0500

    Add support for refreshing the app

--- src/panes/app.js
+++ src/panes/app.js
@@ -1,10 +1,27 @@
+// Module imports
+import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 
 
 
 
+
+// Component constants
+/* eslint-disable import/no-extraneous-dependencies */
+const {
+  getCurrentWindow,
+  globalShortcut,
+} = require('electron').remote
+/* eslint-enable */
+
+
+
+
+
+const reload = () => {
+  getCurrentWindow().reload()
+}
 
 const render = () => {
   /* eslint-disable global-require */
@@ -13,6 +30,18 @@ const render = () => {
 
   ReactDOM.render(<AppContainer><Preferences /></AppContainer>, document.querySelector('#root'))
 }
+
+globalShortcut.register('F5', reload)
+globalShortcut.register('CommandOrControl+R', reload)
+
+
+
+
+
+window.addEventListener('beforeunload', () => {
+  globalShortcut.unregister('F5', reload)
+  globalShortcut.unregister('CommandOrControl+R', reload)
+})
 
 
 
